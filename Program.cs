2dd964caa7cb7876cf8024f22da18ffc101bc004
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2023-03-12 14:43:31 +0530

    added code to update the module twin in IoT Hub

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace DeviceTwinBackend
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+
+        // CHANGE THE CONNECTION STRING TO THE ACTUAL CONNETION STRING OF THE IOT HUB (SERVICE POLICY) 
+        static string connectionString="HostName=ioth.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=tOqSnXV1zygNGAHzAaTSEH/dq3nkWWZmwbSPhesIBO4=";        
+        
+        public static async Task SetDeviceTags()  {
+            var twin=await registryManager.GetTwinAsync("dev-group-1", "module-1");
+            var patch=
+                @"{
+                        properties: {
+                            desired:  {
+                                speedAlertFrom: 120
+                            }
+                        }
+                    }";
+            await registryManager.UpdateTwinAsync(twin.DeviceId, twin.ModuleId, patch, twin.ETag);                    
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Device Twin backend...");
+            registryManager=RegistryManager.CreateFromConnectionString(connectionString);
+            SetDeviceTags().Wait();
+            Console.WriteLine("Hit Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
