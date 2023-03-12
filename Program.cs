a3baa7fadfa5831478ffd7b43e8ae0b7bc54f2a8
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2023-03-12 14:44:17 +0530

    updated

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace DeviceTwinBackend
         static RegistryManager registryManager;
 
         // CHANGE THE CONNECTION STRING TO THE ACTUAL CONNETION STRING OF THE IOT HUB (SERVICE POLICY) 
-        static string connectionString="HostName=ioth.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=tOqSnXV1zygNGAHzAaTSEH/dq3nkWWZmwbSPhesIBO4=";        
+        static string connectionString="service connection string ";        
         
         public static async Task SetDeviceTags()  {
             var twin=await registryManager.GetTwinAsync("dev-group-1", "module-1");
